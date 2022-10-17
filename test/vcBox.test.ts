1b4c060ed0f7c8feed1d2f12a1100f5aae6827ae
Robertosnap <robertosnap@pm.me>
2022-10-17 11:28:44 +0200

    Fix

--- test/vcBox.test.ts
+++ test/vcBox.test.ts
@@ -4,7 +4,6 @@ import { VCBox } from "../src/index.js";
 import { config } from "dotenv";
 
 test.before("load env", (t) => {
-	console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 	if (process.env.NODE_ENV === "test") {
 		t.pass("In test env");
 	} else {
